7957d4ae3aa2166e36ddc07fbaca21dc2ea9a4e4
simonzen <semborfo@gmail.com>
2025-05-05 00:09:11 +0200

    added code and updated config

--- config.tmpl.py
+++ config.tmpl.py
@@ -4,5 +4,12 @@ config = {
             "max_value": 0,
             "min_value": 0,
         }
-    }
+    },
+    "battery": {
+        "r1": 100000,
+        "r2": 100000,
+        "v_min": 3.0,
+        "v_max": 4.2,
+        "v_ref": 0,
+    },
 }
--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import time
 from config import config
 
 
+def battery_charge_level():
+    raw = battery_adc.read()
+    voltage = raw / 4095 * V_REF
+    battery_voltage = voltage * (R1 + R2) / R2
+    battery_percentage = (battery_voltage - V_MIN) / (V_MAX - V_MIN) * 100
+    battery_percentage = max(0, min(100, battery_percentage))
+    return battery_percentage
+
+
 def read_moisture_percent():
     raw = soil.read()
     raw = max(WET_VALUE, min(raw, DRY_VALUE))
@@ -17,6 +26,26 @@ soil = ADC(Pin(0))
 soil.atten(ADC.ATTN_11DB)
 soil.width(ADC.WIDTH_12BIT)
 
+R1 = config["battery"]["r1"]
+R2 = config["battery"]["r2"]
+V_MIN = config["battery"]["v_min"]
+V_MAX = config["battery"]["v_max"]
+V_REF = config["battery"]["v_ref"]
+
+battery_adc = ADC(Pin(1))
+battery_adc.atten(ADC.ATTN_11DB)
+battery_adc.width(ADC.WIDTH_12BIT)
+
 while True:
-    print(read_moisture_percent())
+    test_data = {
+        "data": {
+            "soil": {
+                "moisture": read_moisture_percent(),
+            },
+            "battery": {
+                "percentage": battery_charge_level(),
+            }
+        }
+    }
+    data = read_moisture_percent()
     time.sleep(1)
