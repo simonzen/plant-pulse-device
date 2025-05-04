9915ab897368f4798e401b699b4e6faa3dd6bc6f
simonzen <semborfo@gmail.com>
2025-05-05 00:47:16 +0200

    added code and updated config

--- config.tmpl.py
+++ config.tmpl.py
@@ -1,15 +1,15 @@
 config = {
     "soil": {
         "moisture": {
-            "max_value": 0,
-            "min_value": 0,
+            "max_value": 0,  # ADC value for dry soil
+            "min_value": 0,  # ADC value for dry soil
         }
     },
     "battery": {
-        "r1": 100000,
-        "r2": 100000,
-        "v_min": 3.0,
-        "v_max": 4.2,
-        "v_ref": 0,
+        "r1": 100000,      # Top resistor in voltage divider (ohms)
+        "r2": 100000,      # Bottom resistor in voltage divider (ohms)
+        "v_min": 3.0,      # Voltage corresponding to 0% battery
+        "v_max": 4.2,      # Voltage corresponding to 100% battery
+        "v_ref": 0,        # Calibrated ADC reference voltage (based on measurement)
     },
 }
