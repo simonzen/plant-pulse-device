4ef3b93ade72a5a2bbe0c1e80e47ca0a9bc2d9d0
simonzen <semborfo@gmail.com>
2025-05-08 00:42:57 +0200

    updated readme and added wifi

--- config.tmpl.py
+++ config.tmpl.py
@@ -1,23 +1,23 @@
 config = {
     "network": {
         "wifi": {
-            "ssid": "",
-            "password": "",
+            "ssid": "",         # Your Wi-Fi SSID
+            "password": "",     # Your Wi-Fi password
         },
-        "url": "",
-        "sleep_time": 60*60,
+        "url": "",              # Server endpoint for POST requests
+        "sleep_time": 60*60,    # Delay between measurements (in seconds)
     },
     "soil": {
         "moisture": {
-            "max_value": 0,  # ADC value for dry soil
-            "min_value": 0,  # ADC value for dry soil
+            "max_value": 0,     # ADC value for completely dry soil
+            "min_value": 0,     # ADC value when sensor fully wet (e.g. water cup)
         }
     },
     "battery": {
-        "r1": 100000,  # Top resistor in voltage divider (ohms)
-        "r2": 100000,  # Bottom resistor in voltage divider (ohms)
-        "v_min": 3.0,  # Voltage corresponding to 0% battery
-        "v_max": 4.2,  # Voltage corresponding to 100% battery
-        "v_ref": 0,    # Calibrated ADC reference voltage (based on measurement)
+        "r1": 100000,           # Top resistor in voltage divider (Ohms)
+        "r2": 100000,           # Bottom resistor in voltage divider (Ohms)
+        "v_min": 3.0,           # Battery voltage representing 0% charge
+        "v_max": 4.2,           # Battery voltage representing 100% charge
+        "v_ref": 0,             # Calibrated ADC reference voltage
     },
 }
